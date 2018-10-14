ef664564ff5c0f42c815796cd9bb0718f1da5827
Peter C <petexc@Lancelot.localdomain>
2018-10-14 22:08:12 +0100

    Added random string generator

---
+++ app.js
@@ -0,0 +1,10 @@
+var gRandomString = (length) => {
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+console.log(gRandomString(28));
